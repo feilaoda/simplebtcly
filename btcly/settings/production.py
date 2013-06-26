6c03551c0d0133b4b670a50eaff176a5133a95da
feilaoda <azhenglive@gmail.com>
2013-06-26 03:57:00 -0400

    rm compress

--- btcly/settings/production.py
+++ btcly/settings/production.py
@@ -7,12 +7,12 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 # TODO: I recommend using psycopg2 w/ postgres but sqlite3 is good enough.
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(SITE_ROOT, 'production.db'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(SITE_ROOT, 'production.db'),
+#    }
+#}
 
 # TODO: Be sure to set this.
-SECRET_KEY = ''
+SECRET_KEY = '!#$$.bitcoinly.com'
